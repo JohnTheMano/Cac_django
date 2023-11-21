5ac25ca2cb16360f26b667b8ea1968f7fecd58c8
JohnTheMano <pablo.delapuente@gmail.com>
2023-11-21 16:55:00 -0300

    Avances Presentación Final

--- proyecto_grupal_django/core/urls.py
+++ proyecto_grupal_django/core/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path, re_path
 from . import views
+from django.contrib.auth import views as auth_views
 from .views import ReportesView
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('nosotros/', views.nosotros , name='nosotros'),
     path('usuarios/<str:usuario>', views.nombre_usuario, name='nombre_usuario'),
     path('vehiculos/', views.vehiculos_todos, name='vehiculos_todos'),
+
+    path('vehiculos/registro_exitoso/', views.registro_exitoso, name='registro_exitoso'),
+
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='core/login.html'),name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     
     re_path('vehiculos/anio/(?P<anio>[0-9]{4})/$', views.vehiculos_anio, name='vehiculos_anio'),
     path('vehiculos/disponibles', views.vehiculos_estado, {'estado': 'disponible'}, name="vehiculos_disponible"),
--- proyecto_grupal_django/proyecto_grupal_django/settings.py
+++ proyecto_grupal_django/proyecto_grupal_django/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "vehiculos",
         "USER": "postgres",
-        #"PASSWORD": "1234",
-        "PASSWORD": "37842823",
+        "PASSWORD": "1234",
+        # "PASSWORD": "37842823",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index"
